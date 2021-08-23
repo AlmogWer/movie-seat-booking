ef003dc17e96db9dfad93b47c8506b97d80c2dd4
Almog Wertzberger <Almogish@gmail.com>
2021-08-23 16:32:23 +0300

    add local storage

--- script.js
+++ script.js
@@ -5,18 +5,35 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem(`selectedMovieIndex`, movieIndex);
+  localStorage.setItem(`selectedMoviePrice`, moviePrice);
+}
+
 //Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(`.row .seat.selected`);
   const selectedSeatscount = selectedSeats.length;
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   count.innerText = selectedSeatscount;
   total.innerText = selectedSeatscount * ticketPrice;
+
+  //Copy selected seats into arr
+  //Map through array
+  //return a new array of indexes
 }
 
 //Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
